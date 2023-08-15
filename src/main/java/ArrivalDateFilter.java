7528acdc8399ffa81bb7857b4a9e173638b7b5ee
Vadim <h4998998482@gmail.com>
2023-08-15 23:12:15 +0300

    adding filters

---
+++ src/main/java/ArrivalDateFilter.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class ArrivalDateFilter implements Filter {
+    @Override
+    public List<Flight> filter(List<Flight> flights) {
+        return null;
+    }
+}
---
+++ src/main/java/DurationFilter.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class DurationFilter implements Filter {
+    @Override
+    public List<Flight> filter(List<Flight> flights) {
+        return null;
+    }
+}
